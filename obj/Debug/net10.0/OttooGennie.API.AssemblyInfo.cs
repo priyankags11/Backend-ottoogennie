9e523e3fa8135676afc88c7a7854916ae070d496
Priyanka G S <priyaanju118@gmail.com>
2026-04-15 09:47:33 +0530

    Added Dockerfile

--- obj/Debug/net10.0/OttooGennie.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/OttooGennie.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OttooGennie.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a742fc161aa7a05ceb7556c5464c7a9b470ad260")]
 [assembly: System.Reflection.AssemblyProductAttribute("OttooGennie.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OttooGennie.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
