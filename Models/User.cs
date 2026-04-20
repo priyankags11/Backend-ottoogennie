eb89c698e749191ea67050a8f19323523681c933
Priyanka G S <priyaanju118@gmail.com>
2026-04-20 23:04:17 +0530

    bug fix

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,12 @@
-
 public class User
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-
-
     public string? Name { get; set; }
-
-
     public string? PhoneNumber { get; set; }
-
+    public string? Email { get; set; }
     public string? City { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 }
